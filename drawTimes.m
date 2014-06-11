09fdde7fa9c5887514b8221475c52a2c5e479de7
Mateusz Galazyn <Mateusz.Galazyn@gmail.com>
2014-06-12 00:14:20 +0200

    added ell for drawTimes

--- drawTimes.m
+++ drawTimes.m
@@ -6,32 +6,56 @@ dataCnt = 1;
 simNumber = 69;
 simulationsPath = '/mnt/tesla';
 
-timeIntevals = { 
-    % '0.004',
-    % '0.006',
-    % '0.008',
-    % '0.01',
+timeIntervals = { 
+    '0.004',
+    '0.006',
+    '0.008',
+    '0.01',
     '0.02',
     '0.04',
     '0.06',
     '0.08',
 };
 
+timeIntervalsLength = length(timeIntervals);
 
-for k = 1:length(timeIntevals)
+for k = 1:timeIntervalsLength
   for j = 1:simNumber
-    directory = [simulationsPath '/hubble' num2str(j-1) 't' timeIntevals{k}];
+    directory = [simulationsPath '/hubble' num2str(j-1) 't' timeIntervals{k}];
     simulationTimes = load([directory '/time.dat']);
-    time(k,j) = simulationTimes(5) + simulationTimes(6);
+    hubbleTime(k,j) = simulationTimes(5) + simulationTimes(6);
     if (k == 1)
       dimx(k,j) = simulationTimes(2);
     end
   end
 end
 
+for k = 1:timeIntervalsLength
+  for j = 1:simNumber
+    directory = [simulationsPath '/ellipsoidal' num2str(j-1) 't' timeIntervals{k}];
+    simulationTimes = load([directory '/time.dat']);
+    ellipsoidalTime(k,j) = simulationTimes(5) + simulationTimes(6);
+  end
+end
 
-plot(dimx, time);
-legend(timeIntevals);
+for k = 1:(2*timeIntervalsLength)
+    prefix = '';
+    if(floor(k/timeIntervalsLength) < 1)
+        prefix = 'hub';
+    else
+        prefix = 'ell';
+    end
+
+    legends{k} = [prefix ' ' timeIntervals{mod(k-1,timeIntervalsLength)+1}];
+    k
+    mod(k,timeIntervalsLength)
+    floor(k/timeIntervalsLength)
+
+end
+
+
+plot(dimx, hubbleTime, '-.', dimx, ellipsoidalTime, '-');
+legend(legends);
 xlabel('x dim');
 ylabel('time [s]');
 title('Simulation time (100 iterations) versus cube size');
